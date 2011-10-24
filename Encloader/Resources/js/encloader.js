86ef416ac101a16ce678aa80e8c276c985e663a0
Reid Ransom <reid@reidransom.com>
2011-10-23 23:43:12 -0700

    hardcoded Handbrake encoding presets

--- Encloader/Resources/js/encloader.js
+++ Encloader/Resources/js/encloader.js
@@ -138,11 +138,27 @@ $(function(){
             Titanium.Filesystem.getDesktopDirectory()
           )
         },
+        /*
         {
           "id": "owk93k",
           "type": "ENC",
           "name": "libx264-ultrafast deinterlace",
           "cmd": "{{ffmpeg}} -i {{infile}} -threads {{threads}} -vcodec libx264 -fpre {{ffpresets}}libx264-ultrafast.ffpreset -strict experimental -filter yadif {{outfile}}",
+          "extension": "mp4"
+        },
+        */
+        {
+          "id": "ksh02I",
+          "type": "ENC",
+          "name": "Handbrake 540p 16:9",
+          "cmd": "{{handbrake}} -i {{infile}} -o {{outfile}} -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 960 --height 540 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0",
+          "extension": "mp4"
+        },
+        {
+          "id": "a1yN3K",
+          "type": "ENC",
+          "name": "Handbrake 480p 4:3",
+          "cmd": "{{handbrake}} -i {{infile}} -o {{outfile}} -e x264 -q 20.0 -E faac -B 128 -6 dpl2 -R Auto -D 0.0 -f mp4 --width 640 --height 480 --decomb --crop 0:0:0:0 --optimize -m -x cabac=0:ref=2:me=umh:bframes=0:weightp=0:subme=6:8x8dct=0:trellis=0",
           "extension": "mp4"
         }
       ]
