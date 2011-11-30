ea375ed8fc6c9c35d19c4a217f4d4df6e94fd3dd
Reid Ransom <reid@reidransom.com>
2011-11-30 13:20:44 -0800

    separated jobs and processes

--- Encloader/Resources/js/encloader.js
+++ Encloader/Resources/js/encloader.js
@@ -91,16 +91,9 @@ $(function(){
     "encloader.icns").toString());
   
   var binpath = Titanium.Filesystem.getFile(projectRoot, "bin");
-  var bins = {
-    handbrake: Titanium.Filesystem.getFile(projectRoot, "bin", "HandBrakeCLI"),
-    ffmpeg: Titanium.Filesystem.getFile(projectRoot, "bin", "ffmpeg"),
-    ffmpegfs: Titanium.Filesystem.getFile(projectRoot, "bin", "ffmpegfs"),
-    dnx175mxf: Titanium.Filesystem.getFile(projectRoot, "bin", "dnx175mxf"),
-    qtfaststart: Titanium.Filesystem.getFile(projectRoot, "bin", "qtfaststart.py")
-  };
-  _.each(_.values(bins), function(bin) {
-    if (!bin.isExecutable()) {
-      Titanium.Process.createProcess(["/bin/chmod", "755", bin.toString()]).launch();
+  _.each(binpath.getDirectoryListing(), function(file) {
+    if (!file.isExecutable()) {
+      Titanium.Process.createProcess(["/bin/chmod", "755", file.toString()]).launch();
     }
   });
   
@@ -151,41 +144,30 @@ $(function(){
         {
           "id": "owk93k",
           "type": "ENC",
-          "name": "x264 Ultrafast",
-          "cmd": '{{ffmpegfs}} -i "{{infile}}" -threads {{threads}} -vcodec libx264 -fpre {{ffpresets}}libx264-ultrafast.ffpreset -strict experimental -filter yadif -y "{{outfile}}"',
-          "extension": ".mp4"
+          "name": "ffmpeg Ultrafast x264",
+          "cmd": ["ffmpeg", "-i", "{{infile}}", "-threads", "{{threads}}", "-vcodec", "libx264", "-coder", "0", "-flags", "-loop", "-cmp", "+chroma", "-partitions", "-parti8x8-parti4x4-partp8x8-partb8x8", "-me_method", "dia", "-subq", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "0", "-i_qfactor", "0.71", "-b_strategy", "0", "-qcomp", "0.6", "-qmin", "10", "-qmax", "51", "-qdiff", "4", "-bf", "0", "-refs", "1", "-directpred", "1", "-trellis", "0", "-flags2", "-bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree", "-wpredp", "0", "-acodec", "aac", "-ac", "2", "-b:a", "128k", "-r:a", "48k", "-strict", "experimental", "{{outfile}}.mp4"],
         },
         {
-          "id": "lI3Us0",
+          "id": "k8Ea25",
           "type": "ENC",
-          "name": "x264 HQ",
-          "cmd": '{{ffmpegfs}} -i "{{infile}}" -threads {{threads}} -vcodec libx264 -fpre {{ffpresets}}libx264-hq.ffpreset -strict experimental -filter yadif -y "{{outfile}}"',
-          "extension": ".mp4"
-        },
-        {
-          "id": "lI3Us0",
-          "type": "ENC",
-          "name": "x264 iphone",
-          "cmd": "{{ffmpegfs}} -i {{infile}} -threads {{threads}} -vcodec libx264 -fpre {{ffpresets}}libx264-iphone.ffpreset -strict experimental -filter yadif -s 640x360 -y {{outfile}}",
-          "extension": ".mp4"
+          "name": "B2 Webpost HD",
+          "cmd": [
+            ["ffmpeg", "-i", "{{infile}}", "-crf", "26", "-flags", "+loop+mv4", "-cmp", "256", "-partitions", "+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8", "-me_method", "hex", "-subq", "7", "-trellis", "1", "-refs", "5", "-bf", "0", "-flags2", "+mixed_refs", "-coder", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "40", "-i_qfactor", "0.71", "-qmin", "10", "-qmax", "51", "-acodec", "aac", "-ab", "128", "-ac", "2", "-ar", "48000", "-strict", "experimental", "-vf", 'yadif,scale=960:540', "-threads", "{{threads}}", "{{outfile}}.mp4"],
+            ["qtfaststart.py", "{{outfile}}.mp4"]
+          ]
         },
         {
           "id": "zIs82L",
           "type": "ENC",
           "name": "DNx175 MXF",
-          "cmd": "{{dnx175mxf}} -i {{infile}} -o {{outfile}} -t {{threads}}",
-          "extension": ""
-        }
-        /*{
-          "id": "H8s9Ja",
-          "type": "Q",
-          "name": "queue",
+          "cmd": "dnx175mxf -i {{infile}} -o {{outfile}} -t {{threads}}",
           "cmd": [
-            "{{ffmpeg}} -i {{infile}} -threads {{threads}} -vcodec libx264 -fpre {{ffpresets}}libx264-iphone.ffpreset -strict experimental -filter yadif -s 640x360 -y {{outfile}}",
-            "{{qtfaststart}} {{outfile}}"
+            ["ffmpeg", "-i", "{{infile}}", "-vcodec", "dnxhd", "-b:v", "175M", "-pix_fmt", "yuv422p", "-s", "1920x1080", "-acodec", "pcm_s16le", "-ar", "48000", "-ac", "2", "-threads", "{{threads}}", "-vf", "crop=in_w:in_h-8:0:0,scale=1920:1080", "{{tempfile}}.mov"],
+            ["ffmpeg", "-i", "{{tempfile}}.mov", "-an", "-vcodec", "copy", "{{tempfile}}.m2v"],
+            ["ffmpeg", "-i", "{{tempfile}}.mov", "-vn", "-acodec", "pcm_s16le", "{{tempfile}}.wav"],
+            ["writeavidmxf", "--prefix", "{{outfile}}", "--film23.976", "--DNxHD1080p175", "{{tempfile}}.m2v", "--wavpcm", "{{tempfile}}.wav"]
           ],
-          "extension": "mp4"
-        }*/
+        }
       ]
     });
 
@@ -250,21 +232,20 @@ $(function(){
     }
   
   });
-  var jobq = JobQueue();
+  var jobq = JobQueue([]);
   
-  var JobBase = Class.$extend({
+  var JobView = Class.$extend({
 
-    __init__: function(title) {
+    __init__: function(job) {
       
-      if (title === undefined) title = "";
+      this.job = job;
       
       this.percent = 0;
       this.state = "Pending...";
-      this.title = title;
       
       this.el = $(document.createElement("div"));
       this.el.html(this.template({
-        title: this.title,
+        title: ospath.basename(this.job.infile),
         state: this.state,
         percent: this.percent
       }));
@@ -272,6 +253,14 @@ $(function(){
       this.el_state = this.el.find("p.state");
       this.el_progress = this.el.find("div.progress-bar div");
       this.el_output = this.el.find("div.output");
+      this.el_kill = this.el.find("a.close");
+
+      var jobview = this;
+      this.el_kill.click(function() {
+        publish("/job/killed", [jobview.job]);
+      });
+      
+      $("div.jobs").prepend(this.el);
     
     },
 
@@ -284,11 +273,22 @@ $(function(){
     setPercent: function(val) {
       this.percent = val;
       this.el_progress.css({width: this.percent+"%"});
+      if (this.percent == 100) {
+        this.setState("Done.");
+      }
     },
 
     setState: function(val) {
       this.state = val;
       this.el_state.html(this.state);
+    },
+
+    startUnknown: function() {
+      this.setPercent(0);
+    },
+
+    endUnknown: function() {
+      this.setPercent(100);
     },
 
     addOutput: function(line) {
@@ -299,16 +299,290 @@ $(function(){
   });
   
       
-  /*
+  var LinkedList = Class.$extend({
+  
+    __init__: function() {
+      this.length = 0;
+      this.first = null;
+      this.last = null;
+    },
 
-    Todo: write a titanium function that executes a command line program.
-    Ideally you just pass it a command as a string.  The command could be
-    passed directly to python for a non-blocking system call or it could
-    be stored in a database to be accessed by a helper program later, like
-    helperprogram -j 153, where sqlite job table id 153 is a command...
-    or something like that.
+    append: function(node) {
+      node.next = null;
+      if (this.first === null) {
+        this.first = node;
+        this.last = node;
+      }
+      else {
+        this.last.next = node;
+        this.last = node;
+      }
+      this.length++;
+    }
+  
+  });
+  
+  var ProcessBase = Class.$extend({
+    
+    __init__: function(cmd, job) {
+      this.job = job; // todo: change el to job.view
+      this.process = Titanium.Process.createProcess(cmd, {
+        "PATH": binpath.toString() + ":/usr/bin:/bin"
+      });
+      this.el = job.view;
+      var p = this;
+      this.process.setOnReadLine(function(data){
+        p.el.addOutput(data.toString());
+      });
+      this.process.setOnExit(function() {
+        p.el.setPercent(100);
+        publish("/process/finished", [p]);
+      });
+    },
 
-  */
+    launch: function() {
+      this.el.addOutput(this.process.toString());
+      this.process.launch();
+      this.el.setState("Running...");
+    },
+
+    kill: function() {
+      this.process.kill();
+      this.el.setState("Canceled.");
+    }
+
+  });
+
+  var FFmpegProcess = ProcessBase.$extend({
+    
+    __init__: function(cmd, job) {
+      
+      this.$super(cmd, job);
+      
+      var p = this;
+      var duration_re = /Duration: (\d\d):(\d\d):(\d\d)\.(\d\d)/g;
+      var duration = 0;
+      var time_re = /time=(\d\d):(\d\d):(\d\d)\.(\d\d)/g;
+      var tcTupleToSeconds = function(tc) {
+        return tc[0]*60*60 + tc[1]*60 + tc[2]*1 + tc[3]*.01;
+      };
+      this.process.setOnReadLine(function(data){
+        var line = data.toString();
+        p.el.addOutput(line);
+        if (!duration) {
+          var match = duration_re.exec(line);
+          if (match) {
+            duration = tcTupleToSeconds(match.slice(1));
+          }
+        }
+        else {
+          var match = time_re.exec(line);
+          if (match) {
+            var secs = tcTupleToSeconds(match.slice(1));
+            p.el.setPercent(secs / duration * 100);
+          }
+        }
+      });
+    
+    }
+  
+  });
+  // todo: register this process type
+  
+  // Returns Titanium.Filesystem.File outfile
+  var getOutputFile = function(infile, xtrapath) {
+    
+    var outfile = [Titanium.Filesystem.getDesktopDirectory().toString()];
+    
+    if (xtrapath) {
+      outfile.push(xtrapath);
+    }
+    
+    // If no basename is specified, use the input file basename.
+    if ((!xtrapath) || (xtrapath.slice(-1) == '/')) {
+      // Remove the file extension.
+      infile = infile.slice(0, infile.lastIndexOf("."));
+      outfile.push(ospath.basename(infile));
+    }
+    
+    outfile = Titanium.Filesystem.getFile(outfile);
+    return outfile;
+  
+  };
+      
+  // Returns Titanium.Filesystem.File outfile
+  var getOutputFile_bak = function(infile, xtrapath, extension) {
+    
+    var outfile = [Titanium.Filesystem.getDesktopDirectory().toString()];
+    
+    if (xtrapath) {
+      outfile.push(xtrapath);
+    }
+    
+    // If no basename is specified, use the input file basename.
+    if ((!xtrapath) || (xtrapath.slice(-1) == '/')) {
+      // If the extension is wrong, replace it.
+      if (infile.slice(infile.lastIndexOf(".")) != extension) {
+        infile = infile.slice(0, infile.lastIndexOf(".")) + extension;
+      }
+      outfile.push(ospath.basename(infile));
+    }
+    
+    outfile = Titanium.Filesystem.getFile(outfile);
+    
+    // If the extension is wrong, append it.
+    var outext = outfile.extension();
+    if ("." + outext != extension) {
+      outfile = Titanium.Filesystem.getFile(outfile.toString() + extension);
+    }
+    
+    return outfile;
+  
+  };
+      
+  var mapTemplate = function(list, context) {
+    // Substitute placeholders in command arguments
+    return _.map(list, function(item) {
+      return _.template(item)(context);
+    });
+  }
+
+  var NewJob = LinkedList.$extend({
+  
+    __init__: function(infile, encoder_id, xtrapath) {
+      
+      // A job is a list of processes
+      this.$super();
+      
+      this.infile = infile;
+      
+      this.view = JobView(this);
+
+      var cmds = Presets[encoder_id].cmd;
+      
+      // Incase there is only one command, make a list of one command anyway.
+      if (typeof(cmds[0]) === "string") {
+        cmds = [cmds];
+      }
+      
+      // Create argument subsitution variables.
+      this.tempdir = Titanium.Filesystem.getApplicationDataDirectory();
+      this.tempbase = random.string(6);
+      subs = {
+        infile: infile,
+        outfile: getOutputFile(infile, xtrapath).toString(),
+        threads: Titanium.Platform.getProcessorCount() + "",
+        tempfile: Titanium.Filesystem.getFile(this.tempdir, this.tempbase)
+      };
+      
+      // Get the outfiles and tempfiles
+      this.outfiles = [];
+      this.tempfiles = [];
+      _.each(_.flatten(cmds), function(arg) {
+        // todo: make the actual subsitutions here as well
+        if (arg.match(/{{outfile}}/)) {
+          this.outfiles.push(_.template(arg)(subs));
+        }
+        if (arg.match(/{{tempfile}}/)) {
+          this.tempfiles.push(_.template(arg)(subs));
+        }
+      }, this);
+      this.outfiles = _.uniq(this.outfiles);
+      this.tempfiles = _.uniq(this.tempfiles);
+      
+      // Make sure these files don't already exist.
+      _.each(this.outfiles.concat(this.tempfiles), function(file) {
+        file = Titanium.Filesystem.getFile(file);
+        if (file.exists()) {
+          this.view.setState("File already exists: " + file.toString());
+        }
+        // todo: check against other files to be created during the batch.
+      }, this);
+
+      // Subsitute the arguments.
+      cmds = _.map(cmds, function(cmd) {
+        return mapTemplate(cmd, subs);
+      });
+
+      // Add each process to this job
+      for (var i=0; i<cmds.length; i++) {
+        var process = FFmpegProcess(cmds[i], this);
+        this.append(process);
+      }
+
+      // There is no currently running process
+      this.current = null;
+
+      // Killed flag
+      this.killed = 0;
+      
+    },
+
+    launch: function() {
+      this.current = this.first;
+      this.first.launch();
+    }
+
+  });
+  
+  subscribe("/job/killed", function(job) {
+    job.killed = 1;
+    job.view.setState("Cancelled.");
+    job.current.kill();
+  });
+  
+  subscribe("/process/finished", function(process) {
+    
+    // If there is another process to run... launch it.
+    if ((process.next) && (!process.job.killed)) {
+      process.current = process.next;
+      process.next.launch();
+    }
+    
+    else {
+      
+      // There are no more currently running processes.
+      process.current = null;
+      
+      // Remove tempfiles
+      _.each(process.job.tempfiles, function(file) {
+        file.deleteFile();
+      });
+      
+      // Let the job queue know this finished
+      publish("/job/finished", []);
+    
+    }
+  });
+
+  // Wait for files to be dropped on the dropzone
+  var initDropzone = function(dropzone) {
+    
+    window.setInterval(function() {
+      var files = dropzone.val();
+      if (files) {
+        
+        // Clear the dropzone value for the next batch of files to be dropped.
+        dropzone.val("");
+
+        // Get the currently selected form values.
+        var encoder_id = select.encoders.val();
+        var uploader_id = select.uploaders.val();
+        var path = $("#path").val();
+        
+        // Create a job for each file that was dropped.
+        _(files.split("\n")).each(function(file) {
+          //var j = Job(file, encoder_id, uploader_id, path);
+          var j = NewJob(file, encoder_id, path);
+          jobq.addJob(j);
+        });
+        
+      }
+    }, 500);
+  
+  };
+  initDropzone($("textarea.dropzone"));
+  
   var Job = Class.$extend({
   
     __init__: function(infile, encoder_id, uploader_id, path) {
@@ -402,40 +676,24 @@ $(function(){
       this.queue = [];
       
       var enccmd = this.encoder.cmd.split(" ");
+      
       var ffpresets = Titanium.Filesystem.getFile(projectRoot,
         "ffpresets").toString() + separator;
-      var infile = this.infile;
+      
       var bin = "handbrake";
-      _.each(enccmd, function(param, i) {
-        enccmd[i] = param.replace("{{ffpresets}}", ffpresets);
-        enccmd[i] = param.replace("{{outfile}}", localpath.toString());
-        if (param === "{{handbrake}}") {
-          enccmd[i] = bins.handbrake;
-        }
-        else if (param === "{{ffmpeg}}") {
-          enccmd[i] = bins.ffmpeg;
-          bin = "ffmpeg";
-        }
-        else if (param === "{{ffmpegfs}}") {
-          enccmd[i] = "ffmpegfs";
-          enccmd[2] = '"' + infile.toString() + '"';
-          bin = "ffmpeg";
-        }
-        else if (param === "{{dnx175mxf}}") {
-          enccmd[i] = "dnx175mxf";
-          bin = "ffmpeg";
-        }
-        else if (param === "{{qtfaststart}}") {
-          enccmd[i] = bins.qtfaststart;
-        }
-        else if (param === "{{threads}}") {
-          enccmd[i] = Titanium.Platform.getProcessorCount() + "";
-        }
+      
+      var subs = {
+        ffpresets: ffpresets,
+        outfile: localpath.toString(),
+        infile: infile.toString(),
+        threads: Titanium.Platform.getProcessorCount() + ""
+      };
+      enccmd = _.map(enccmd, function(arg) {
+        return _.template(arg)(subs);
       });
-
-  //var ProcessBase = Class.$extend({
-    
-  //});
+      
+      this.job.addOutput(enccmd);
+      
       this.process = Titanium.Process.createProcess(enccmd, {
         'PATH': binpath.toString() + ":/usr/bin:/bin"
       });
@@ -498,11 +756,9 @@ $(function(){
         
         x.job.setPercent(100);
         if (x.uploader.type === "MV") {
-          x.job.setState("Done.");
           publish("/job/finished", []);
           return;
         }
-        x.job.setState("Done. (task 1 of 2)");
         
         var upcmd = [
           "curl",
@@ -526,7 +782,6 @@ $(function(){
           }
         });
         upprocess.setOnExit(function() {
-          x.job.setState("Done.");
           x.job.setPercent(100);
           publish("/job/finished", []);
         });
@@ -560,29 +815,6 @@ $(function(){
 
   });
 
-  
-  // Wait for files to be dropped on the dropzone
-  var initDropzone = function(dropzone) {
-  
-    window.setInterval(function() {
-      var files = dropzone.val();
-      if (files) {
-        dropzone.val("");
-        var encoder_id = select.encoders.val();
-        var uploader_id = select.uploaders.val();
-        var path = $("#path").val();
-        
-        _(files.split("\n")).each(function(file) {
-          var j = Job(file, encoder_id, uploader_id, path);
-          jobq.addJob(j);
-        });
-        
-      }
-    }, 500);
-  
-  };
-  
-  initDropzone($("textarea.dropzone"));
   
 
   
