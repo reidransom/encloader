a4598537c8f489c0f828b00b19d06e92ebdd9439
Reid Ransom <reid@reidransom.com>
2012-01-21 22:28:39 -0800

    lets you know when outfile exists

--- Encloader/Resources/js/encloader.js
+++ Encloader/Resources/js/encloader.js
@@ -3,19 +3,14 @@ $(function(){
   var JOBS = 3;
   
   // Cache selects
-  var select = {
-    encoders: $("select.encoders"),
-    uploaders: $("select.uploaders")
-  };
+  var select_presets = $("select.encoders");
 
   var getPresets = function() {
     
     var presets = {};
     
     // Clear the select elements
-    _(select).each(function(s) {
-      s.html("");
-    });
+    select_presets.html("");
 
     var sources = [];
     
@@ -37,33 +32,30 @@ $(function(){
       "name": "Built-in",
       "presets": [
         {
-          "id": "kej83J",
-          "type": "MV",
-          "name": "Desktop",
-          "path": Titanium.Filesystem.getFile(
-            Titanium.Filesystem.getDesktopDirectory()
-          )
+          "id": "ekn39s",
+          "type": "ENC",
+          "name": "Sundance online h.264",
+          "cmd": ["ffmpeg", "-i", "{{infile}}", "-threads", "{{threads}}", "-vcodec", "libx264", "-coder", "0", "-flags", "-loop", "-cmp", "+chroma", "-partitions", "-parti8x8-parti4x4-partp8x8-partb8x8", "-me_method", "dia", "-subq", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "0", "-i_qfactor", "0.71", "-b_strategy", "0", "-qcomp", "0.6", "-qmin", "10", "-qmax", "51", "-qdiff", "4", "-bf", "0", "-refs", "1", "-directpred", "1", "-trellis", "0", "-flags2", "-bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree", "-wpredp", "0", "-acodec", "aac", "-ac", "2", "-b:a", "256k", "-r:a", "48k", "-strict", "experimental", "-vf", "yadif,scale=1024:576", "{{outfile}}.mov"],
         },
-        {
+        /*{
           "id": "owk93k",
           "type": "ENC",
           "name": "ffmpeg Ultrafast x264",
           "cmd": ["ffmpeg", "-i", "{{infile}}", "-threads", "{{threads}}", "-vcodec", "libx264", "-coder", "0", "-flags", "-loop", "-cmp", "+chroma", "-partitions", "-parti8x8-parti4x4-partp8x8-partb8x8", "-me_method", "dia", "-subq", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "0", "-i_qfactor", "0.71", "-b_strategy", "0", "-qcomp", "0.6", "-qmin", "10", "-qmax", "51", "-qdiff", "4", "-bf", "0", "-refs", "1", "-directpred", "1", "-trellis", "0", "-flags2", "-bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree", "-wpredp", "0", "-acodec", "aac", "-ac", "2", "-b:a", "128k", "-r:a", "48k", "-strict", "experimental", "{{outfile}}.mp4"],
-        },
+        },*/
         {
           "id": "k8Ea25",
           "type": "ENC",
           "name": "B2 Webpost HD",
           "cmd": [
-            ["ffmpeg", "-i", "{{infile}}", "-crf", "26", "-flags", "+loop+mv4", "-cmp", "256", "-partitions", "+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8", "-me_method", "hex", "-subq", "7", "-trellis", "1", "-refs", "5", "-bf", "0", "-flags2", "+mixed_refs", "-coder", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "40", "-i_qfactor", "0.71", "-qmin", "10", "-qmax", "51", "-acodec", "aac", "-ab", "128", "-ac", "2", "-ar", "48000", "-strict", "experimental", "-vf", 'yadif,scale=960:540', "-threads", "{{threads}}", "{{outfile}}.mp4"],
+            ["ffmpeg", "-i", "{{infile}}", "-crf", "26", "-flags", "+loop+mv4", "-cmp", "256", "-partitions", "+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8", "-me_method", "hex", "-subq", "7", "-trellis", "1", "-refs", "5", "-bf", "0", "-flags2", "+mixed_refs", "-coder", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "40", "-i_qfactor", "0.71", "-qmin", "10", "-qmax", "51", "-acodec", "aac", "-b:a", "128k", "-ac", "2", "-r:a", "48k", "-strict", "experimental", "-vf", 'yadif,scale=960:540', "-threads", "{{threads}}", "{{outfile}}.mp4"],
             ["qtfaststart.py", "{{outfile}}.mp4"]
           ]
         },
         {
           "id": "zIs82L",
           "type": "ENC",
-          "name": "DNx175 MXF",
-          "cmd": "dnx175mxf -i {{infile}} -o {{outfile}} -t {{threads}}",
+          "name": "5D to DNx175 MXF",
           "cmd": [
             ["ffmpeg", "-i", "{{infile}}", "-vcodec", "dnxhd", "-b:v", "175M", "-pix_fmt", "yuv422p", "-s", "1920x1080", "-acodec", "pcm_s16le", "-ar", "48000", "-ac", "2", "-threads", "{{threads}}", "-vf", "crop=in_w:in_h-8:0:0,scale=1920:1080", "{{tempfile}}.mov"],
             ["ffmpeg", "-i", "{{tempfile}}.mov", "-an", "-vcodec", "copy", "{{tempfile}}.m2v"],
@@ -79,10 +71,9 @@ $(function(){
       _(source.presets).each(function(preset, i) {
         preset["source"] = source.name;
         preset["order"] = i;
-        var type = (preset.type === "ENC") ? "encoders" : "uploaders";
-        select[type].append(html.option(
+        select_presets.append(html.option(
           preset.id,
-          /*preset.source + ' / ' +*/ preset.name
+          preset.name
         ));
         presets[preset.id] = preset;
       });
@@ -323,6 +314,9 @@ $(function(){
 
       var cmds = Presets[encoder_id].cmd;
       
+      // Killed flag
+      this.killed = 0;
+      
       // Incase there is only one command, make a list of one command anyway.
       if (typeof(cmds[0]) === "string") {
         cmds = [cmds];
@@ -358,6 +352,7 @@ $(function(){
         file = Titanium.Filesystem.getFile(file);
         if (file.exists()) {
           this.view.setState("File already exists: " + file.toString());
+          this.killed = 1;
         }
         // todo: check against other files to be created during the batch.
       }, this);
@@ -376,9 +371,6 @@ $(function(){
       // There is no currently running process
       this.current = null;
 
-      // Killed flag
-      this.killed = 0;
-      
     },
 
     launch: function() {
@@ -439,15 +431,17 @@ $(function(){
         dropzone.val("");
 
         // Get the currently selected form values.
-        var encoder_id = select.encoders.val();
-        var uploader_id = select.uploaders.val();
+        var encoder_id = select_presets.val();
         var path = $("#path").val();
         
         // Create a job for each file that was dropped.
         _(files.split("\n")).each(function(file) {
           //var j = Job(file, encoder_id, uploader_id, path);
           var j = NewJob(file, encoder_id, path);
-          jobq.addJob(j);
+          log.debug(j.killed);
+          if (!j.killed) {
+            jobq.addJob(j);
+          }
         });
         
       }
