96f83759ebae438881a136074604867a157a3478
Reid Ransom <reid@reidransom.com>
2012-02-12 20:16:32 -0800

    changed some built-in presets

--- Encloader/Resources/js/encloader.js
+++ Encloader/Resources/js/encloader.js
@@ -31,25 +31,41 @@ $(function(){
     sources.unshift({
       "name": "Built-in",
       "presets": [
-        {
+        /*{
           "id": "ekn39s",
           "type": "ENC",
           "name": "Sundance online h.264",
           "cmd": ["ffmpeg", "-i", "{{infile}}", "-threads", "{{threads}}", "-vcodec", "libx264", "-coder", "0", "-flags", "-loop", "-cmp", "+chroma", "-partitions", "-parti8x8-parti4x4-partp8x8-partb8x8", "-me_method", "dia", "-subq", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "0", "-i_qfactor", "0.71", "-b_strategy", "0", "-qcomp", "0.6", "-qmin", "10", "-qmax", "51", "-qdiff", "4", "-bf", "0", "-refs", "1", "-directpred", "1", "-trellis", "0", "-flags2", "-bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree", "-wpredp", "0", "-acodec", "aac", "-ac", "2", "-b:a", "256k", "-r:a", "48k", "-strict", "experimental", "-vf", "yadif,scale=1024:576", "{{outfile}}.mov"],
         },
-        /*{
+        {
           "id": "owk93k",
           "type": "ENC",
           "name": "ffmpeg Ultrafast x264",
           "cmd": ["ffmpeg", "-i", "{{infile}}", "-threads", "{{threads}}", "-vcodec", "libx264", "-coder", "0", "-flags", "-loop", "-cmp", "+chroma", "-partitions", "-parti8x8-parti4x4-partp8x8-partb8x8", "-me_method", "dia", "-subq", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "0", "-i_qfactor", "0.71", "-b_strategy", "0", "-qcomp", "0.6", "-qmin", "10", "-qmax", "51", "-qdiff", "4", "-bf", "0", "-refs", "1", "-directpred", "1", "-trellis", "0", "-flags2", "-bpyramid-mixed_refs-wpred-dct8x8+fastpskip-mbtree", "-wpredp", "0", "-acodec", "aac", "-ac", "2", "-b:a", "128k", "-r:a", "48k", "-strict", "experimental", "{{outfile}}.mp4"],
         },*/
         {
+          "id": "e8U1qZ",
+          "type": "ENC",
+          "name": "H.264/AAC HQ 960x540 Deinterlace",
+          "cmd": [
+            ["ffmpeg", "-i", "{{infile}}", "-crf", "22", "-flags", "+loop+mv4", "-cmp", "256", "-partitions", "+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8", "-me_method", "hex", "-subq", "7", "-trellis", "1", "-refs", "5", "-bf", "0", "-flags2", "+mixed_refs", "-coder", "0", "-me_range", "16", "-g", "200", "-keyint_min", "25", "-sc_threshold", "40", "-i_qfactor", "0.71", "-qmin", "10", "-qmax", "51", "-acodec", "aac", "-b:a", "256k", "-ac", "2", "-r:a", "48k", "-strict", "experimental", "-vf", 'yadif,scale=960:540', "-threads", "{{threads}}", "{{outfile}}.mp4"],
+            ["qtfaststart.py", "{{outfile}}.mp4"]
+          ]
+        },
+        {
           "id": "k8Ea25",
           "type": "ENC",
-          "name": "B2 Webpost HD",
+          "name": "H.264/AAC 960x540 Deinterlace",
           "cmd": [
             ["ffmpeg", "-i", "{{infile}}", "-crf", "26", "-flags", "+loop+mv4", "-cmp", "256", "-partitions", "+parti4x4+parti8x8+partp4x4+partp8x8+partb8x8", "-me_method", "hex", "-subq", "7", "-trellis", "1", "-refs", "5", "-bf", "0", "-flags2", "+mixed_refs", "-coder", "0", "-me_range", "16", "-g", "250", "-keyint_min", "25", "-sc_threshold", "40", "-i_qfactor", "0.71", "-qmin", "10", "-qmax", "51", "-acodec", "aac", "-b:a", "128k", "-ac", "2", "-r:a", "48k", "-strict", "experimental", "-vf", 'yadif,scale=960:540', "-threads", "{{threads}}", "{{outfile}}.mp4"],
             ["qtfaststart.py", "{{outfile}}.mp4"]
+          ]
+        },
+        {
+          "id": "8wI2L9",
+          "type": "ENC",
+          "name": "Animation",
+          "cmd": ["ffmpeg", "-i", "{{infile}}", "-vcodec", "qtrle", "-g", "1", "-acodec", "pcm_s16le", "{{outfile}}-animation.mov"
           ]
         },
         {
