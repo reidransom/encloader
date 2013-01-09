5fdd14747e993c13b60940fe6029244d45815a57
Reid Ransom <reid@reidransom.com>
2013-01-08 22:35:38 -0800

    added example ~/.encloader folder

---
+++ sample-dot-encloader/bookmarks.js
@@ -0,0 +1,9 @@
+{
+  "Example Site": {
+    "url": "https://example.com/folder/",
+    "server": "ftp.example.com",
+    "username": "ftp-username",
+    "password": "ftp-password",
+    "path": "/folder/"
+  }
+}
